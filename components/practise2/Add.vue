57d0ed5f4a0f02d7b6fa8dae9936667a35960b98
vikram-500apps <vikram.shamakuri@500apps.com>
2023-04-09 19:24:45 +0530

    email templates task

--- components/practise2/Add.vue
+++ components/practise2/Add.vue
@@ -1,89 +1,187 @@
 <template>
-  <div
-    class="mx-auto mt-8 grid max-w-3xl grid-cols-1 gap-6 sm:px-6 lg:max-w-7xl"
-  >
-    <h1 class="font-semibold leading-6 text-gray-900 mb-3 text-2xl">
-      Candidates
-    </h1>
-    <div class="space-y-6 lg:col-span-2 lg:col-start-1">
-      <section aria-labelledby="notes-title">
-        <div class="bg-white shadow sm:overflow-hidden sm:rounded-lg">
-          <div class="divide-y divide-gray-200">
-            <div class="px-4 py-5 sm:px-6">
-              <h2 id="notes-title" class="text-lg font-medium text-gray-900">
-                Mock interviews
-              </h2>
-            </div>
-          </div>
-        </div>
-      </section>
-    </div>
+  <div>
+    <button
+      @click="open = true"
+      type="button"
+      class="mt-2 bg-indigo-600 text-white py-3 px-6 rounded-md shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600 mx-auto block"
+    >
+      Add Lead
+    </button>
 
-    <div>
-      <form
-        action="#"
-        method="POST"
-        class="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48"
-      >
-        <div class="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
-          <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-            <div>
-              <label
-                for="first-name"
-                class="block text-sm font-semibold leading-6 text-gray-900"
-                >enter your name</label
+    <TransitionRoot as="template" :show="open">
+      <Dialog as="div" class="relative z-10" @close="open = false">
+        <div class="fixed inset-0" />
+
+        <div class="fixed inset-0 overflow-hidden">
+          <div class="absolute inset-0 overflow-hidden">
+            <div
+              class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10"
+            >
+              <TransitionChild
+                as="template"
+                enter="transform transition ease-in-out duration-500 sm:duration-700"
+                enter-from="translate-x-full"
+                enter-to="translate-x-0"
+                leave="transform transition ease-in-out duration-500 sm:duration-700"
+                leave-from="translate-x-0"
+                leave-to="translate-x-full"
               >
-              <div class="mt-2.5">
-                <input
-                  v-model="firsht"
-                  type="text"
-                  name="first-name"
-                  id="first-name"
-                  autocomplete="given-name"
-                  class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div>
-              <label
-                for="last-name"
-                class="block text-sm font-semibold leading-6 text-gray-900"
-                >description</label
-              >
-              <div class="mt-2.5">
-                <input
-                  v-model="last"
-                  type="text"
-                  name="last-name"
-                  id="last-name"
-                  autocomplete="family-name"
-                  class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-              <div>
-                <button
-                  @click.prevent="send"
-                  type="submit"
-                  class="rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  save
-                </button>
-              </div>
+                <DialogPanel class="pointer-events-auto w-screen max-w-md">
+                  <div
+                    class="flex h-full flex-col overflow-y-scroll bg-white shadow-xl"
+                  >
+                    <div class="bg-indigo-700 px-4 py-6 sm:px-6">
+                      <div class="flex items-center justify-between">
+                        <DialogTitle
+                          class="text-base font-semibold leading-6 text-white"
+                          >email templates</DialogTitle
+                        >
+                        <div class="ml-3 flex h-7 items-center">
+                          <button
+                            type="button"
+                            class="rounded-md bg-indigo-700 text-indigo-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
+                            @click="open = false"
+                          >
+                            <span class="sr-only">Close panel</span>
+                            <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="relative flex-1 px-4 py-6 sm:px-6">
+                      <div>
+                        <form
+                          action="#"
+                          method="POST"
+                          class="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48"
+                        >
+                          <div class="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
+                            <div
+                              class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2"
+                            >
+                              <div>
+                                <label
+                                  for="first-name"
+                                  class="block text-sm font-semibold leading-6 text-gray-900"
+                                  >enter your name</label
+                                >
+
+                                <div class="mt-2.5">
+                                  <input
+                                    v-model="firsht"
+                                    type="text"
+                                    name="first-name"
+                                    id="first-name"
+                                    autocomplete="given-name"
+                                    class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                  />
+                                </div>
+                              </div>
+                              <div>
+                                <label
+                                  for="first-name"
+                                  class="block text-sm font-semibold leading-6 text-gray-900"
+                                  >subject</label
+                                >
+
+                                <div class="mt-2.5">
+                                  <input
+                                    v-model="fourth"
+                                    type="text"
+                                    name="first-name"
+                                    id="first-name"
+                                    autocomplete="given-name"
+                                    class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                  />
+                                </div>
+                              </div>
+                              <div>
+                                <label
+                                  for="first-name"
+                                  class="block text-sm font-semibold leading-6 text-gray-900"
+                                  >body</label
+                                >
+
+                                <div class="mt-2.5">
+                                  <input
+                                    v-model="third"
+                                    type="text"
+                                    name="first-name"
+                                    id="first-name"
+                                    autocomplete="given-name"
+                                    class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                  />
+                                </div>
+                              </div>
+
+                              <div>
+                                <label
+                                  for="last-name"
+                                  class="block text-sm font-semibold leading-6 text-gray-900"
+                                  >description</label
+                                >
+
+                                <div class="mt-2.5">
+                                  <input
+                                    v-model="last"
+                                    type="text"
+                                    name="last-name"
+                                    id="last-name"
+                                    autocomplete="family-name"
+                                    class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                  />
+                                </div>
+
+                                <button
+                                  @click.prevent="send"
+                                  type="submit"
+                                  class="mt-2 w-32 rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ml-2"
+                                >
+                                  save
+                                </button>
+                              </div>
+                            </div>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </DialogPanel>
+              </TransitionChild>
             </div>
           </div>
         </div>
-      </form>
-      <div></div>
-    </div>
+      </Dialog>
+    </TransitionRoot>
   </div>
 </template>
-<script setup >
+
+<script setup>
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  TransitionChild,
+  TransitionRoot,
+} from "@headlessui/vue";
+import { XMarkIcon } from "@heroicons/vue/24/outline";
 import { ref } from "vue";
+
 import { defineEmits } from "vue";
+const open = ref(false);
 const firsht = ref("");
+const fourth = ref("");
+const third = ref("");
 const last = ref("");
+
 const emit = defineEmits(["inputData"]);
+
 function send() {
-  emit("inputData", { name: firsht, category: last });
+  emit("inputData", {
+    name: firsht,
+    subject: fourth,
+    body: third,
+    category: last,
+  });
 }
 </script>
--- components/practise2/Main.vue
+++ components/practise2/Main.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-    <Practise2Add @inputData="inputData" />
-    <Practise2List :details="details" @edit="editData" />
-    <Practise2Edit :updateData="updateData" @editData="editData" />
+    <div>
+      <Practise2Add @inputData="inputData" />
+      <Practise2List :details="details" @edit="editData" />
+      <Practise2Edit
+        v-if="isEdit"
+        :key="render"
+        :updateData="updateData"
+        @editData="editData"
+      />
+    </div>
   </div>
 </template>
-<script setup>
+<script setup >
 const details = ref("");
+const open = ref("false");
 async function inputData(input) {
   const postoptions = {
     method: "POST",
@@ -15,18 +23,29 @@ async function inputData(input) {
       Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1IjoiN2ZiMzBkNzhmM2NmNGEwZmJiZWNkZjJkOGM2ZjNhMGEiLCJkIjoiMTY4MDA3NyIsInIiOiJzYSIsInAiOiJmcmVlIiwiYSI6ImZpbmRlci5pbyIsImwiOiJ1czEiLCJleHAiOjE2ODMyNzM0NzR9.QjMEQKeWqKdjLekJkiFGTdhJ3iwilHM5Aa9FEqbWvOI`,
     },
     body: {
+      project_id: "string",
       name: input.name,
+      subject: input.subject,
+      body: input.body,
+      is_active: "1",
+      type: "HTML",
+      share_type: "PRIVATE",
       category: input.category,
     },
   };
+
   const data = await useAuthLazyFetchPost(
-    "https://v1-orm-lib.mars.hipso.cc/api/pages/",
+    "https://v1-orm-lib.mars.hipso.cc/api/email-templates/",
     postoptions
   );
   details.value = data.data._rawValue;
 }
+const render = ref(0);
+const isEdit = ref(false);
 const updateData = ref({});
 const editData = (data) => {
+  isEdit.value = true;
+  render.value++;
   updateData.value = data;
   const putoptions = {
     method: "PUT",
@@ -37,16 +56,18 @@ const editData = (data) => {
     body: {
       name: data.name,
       category: data.category,
+      body: data.body,
+      subject: data.subject,
     },
   };
   const putData = useAuthLazyFetchPut(
-    `https://v1-orm-lib.mars.hipso.cc/api/pages/${data.uid}`,
+    `https://v1-orm-lib.mars.hipso.cc/api/email-templates/${data.uid}`,
     putoptions
   );
 };
 
 const getData = useAuthLazyFetch(
-  "https://v1-orm-lib.mars.hipso.cc/api/pages/?offset=0&limit=100&sort_column=id&sort_direction=desc"
+  "https://v1-orm-lib.mars.hipso.cc/api/email-templates/?offset=0&limit=100&sort_column=id&sort_direction=desc"
 );
 details.value = getData.data._rawValue;
 </script>
