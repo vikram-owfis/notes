3c5ebb81a53612394148b68c3cd19cb55c2b1bed
vikram-owfis <126160989+vikram-owfis@users.noreply.github.com>
2023-04-10 15:19:27 +0530

    Update Add.vue

--- components/practise2/Add.vue
+++ components/practise2/Add.vue
@@ -1,151 +1,15 @@
 <template>
   <div>
-    <Dialog as="div" class="relative z-10" @close="open = false">
-      <div class="fixed inset-0" />
-
-      <div class="fixed inset-0 overflow-hidden">
-        <div class="absolute inset-0 overflow-hidden">
-          <div
-            class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10"
-          >
-            <TransitionChild
-              as="template"
-              enter="transform transition ease-in-out duration-500 sm:duration-700"
-              enter-from="translate-x-full"
-              enter-to="translate-x-0"
-              leave="transform transition ease-in-out duration-500 sm:duration-700"
-              leave-from="translate-x-0"
-              leave-to="translate-x-full"
-            >
-              <DialogPanel class="pointer-events-auto w-screen max-w-md">
-                <div
-                  class="flex h-full flex-col overflow-y-scroll bg-white shadow-xl"
-                >
-                  <div class="bg-indigo-700 px-4 py-6 sm:px-6">
-                    <div class="flex items-center justify-between">
-                      <DialogTitle
-                        class="text-base font-semibold leading-6 text-white"
-                        >Exam</DialogTitle
-                      >
-                      <div class="ml-3 flex h-7 items-center">
-                        <button
-                          type="button"
-                          class="rounded-md bg-indigo-700 text-indigo-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
-                          @click="open = false"
-                        >
-                          <span class="sr-only">Close panel</span>
-                          <XMarkIcon class="h-6 w-6" aria-hidden="true" />
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="relative flex-1 px-4 py-6 sm:px-6">
-                    <div>
-                      <form
-                        action="#"
-                        method="POST"
-                        class="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48"
-                      >
-                        <div class="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
-                          <div
-                            class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2"
-                          >
-                            <div>
-                              <label
-                                for="first-name"
-                                class="block text-sm font-semibold leading-6 text-gray-900"
-                                >enter your name</label
-                              >
-
-                              <div class="mt-2.5">
-                                <input
-                                  v-model="updateData.name"
-                                  type="text"
-                                  name="first-name"
-                                  id="first-name"
-                                  autocomplete="given-name"
-                                  class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                              </div>
-                            </div>
-                            <div>
-                              <label
-                                for="first-name"
-                                class="block text-sm font-semibold leading-6 text-gray-900"
-                                >subject</label
-                              >
-
-                              <div class="mt-2.5">
-                                <input
-                                  v-model="updateData.subject"
-                                  type="text"
-                                  name="first-name"
-                                  id="first-name"
-                                  autocomplete="given-name"
-                                  class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                              </div>
-                            </div>
-                            <div>
-                              <label
-                                for="first-name"
-                                class="block text-sm font-semibold leading-6 text-gray-900"
-                                >body</label
-                              >
-
-                              <div class="mt-2.5">
-                                <input
-                                  v-model="updateData.body"
-                                  type="text"
-                                  name="first-name"
-                                  id="first-name"
-                                  autocomplete="given-name"
-                                  class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                              </div>
-                            </div>
-
-                            <div>
-                              <label
-                                for="last-name"
-                                class="block text-sm font-semibold leading-6 text-gray-900"
-                                >description</label
-                              >
-
-                              <div class="mt-2.5">
-                                <input
-                                  v-model="updateData.category"
-                                  type="text"
-                                  name="last-name"
-                                  id="last-name"
-                                  autocomplete="family-name"
-                                  class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                              </div>
-
-                              <button
-                                @click.prevent="send"
-                                type="submit"
-                                class="mt-2 w-32 rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ml-2"
-                              >
-                                save
-                              </button>
-                            </div>
-                          </div>
-                        </div>
-                      </form>
-                    </div>
-                  </div>
-                </div>
-              </DialogPanel>
-            </TransitionChild>
-          </div>
-        </div>
-      </div>
-    </Dialog>
+    <button
+      @click="openModal()"
+      type="button"
+      class="mt-2 bg-indigo-600 text-white py-3 px-6 rounded-md shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600 mx-auto block"
+    >
+      Add Contact
+    </button>
 
     <TransitionRoot as="template" :show="open">
-      <Dialog as="div" class="relative z-10" @close="openModal">
+      <Dialog as="div" class="relative z-10">
         <TransitionChild
           as="template"
           enter="ease-out duration-300"
@@ -176,10 +40,6 @@
               <DialogPanel
                 class="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:max-w-fit sm:max-w-4xl sm:p-4"
               >
-                <!--
-                regularization
-            -->
-
                 <div class="sm:divide-y-0 sm:gap-px sm:grid">
                   <div class="border m-auto p-4">
                     <div class="px-4 sm:px-6 lg:px-8">
@@ -204,7 +64,7 @@
                                   d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
                                 />
                               </svg>
-                              Edit contact
+                              Add Contact
                             </h3>
                           </div>
                           <div class="border-b border-gray-400"></div>
@@ -219,7 +79,7 @@
                                 >
                                 <div class="mt-2.5">
                                   <input
-                                    v-model="updateData.name"
+                                    v-model="firsht"
                                     type="text"
                                     name="first-name"
                                     id="first-name"
@@ -237,7 +97,7 @@
                                 >
                                 <div class="mt-2">
                                   <input
-                                    v-model="updateData.subject"
+                                    v-model="fourth"
                                     type="number"
                                     name="first-name"
                                     id="first-name"
@@ -257,7 +117,7 @@
                             >
                             <div class="mt-2 relative rounded-md shadow-sm">
                               <select
-                                v-model="updateData.category"
+                                v-model="last"
                                 name="cars"
                                 id="cars"
                                 class="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
@@ -275,14 +135,29 @@
                           <div
                             class="mt-4 px-8 flex justify-between items-center"
                           >
+                            <button
+                              type="button"
+                              @click="open = false"
+                              class="bg-red-600 py-1 px-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                              back
+                            </button>
+
                             <div class="flex">
                               <button
-                                @click="editData"
-                                @click.prevent="send"
-                                type="submit"
-                                class="mt-2 w-32 rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ml-2"
+                                type="button"
+                                class="bg-gray-600 mr-12 py-1 px-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                               >
-                                save
+                                Edit
+                              </button>
+
+                              <button
+                                type="submit"
+                                @click.prevent="send"
+                                @click="open = false"
+                                class="bg-indigo-600 py-1 px-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                              >
+                                Save
                               </button>
                             </div>
                           </div>
@@ -310,20 +185,27 @@ import {
   TransitionRoot,
 } from "@headlessui/vue";
 import { XMarkIcon } from "@heroicons/vue/24/outline";
-import { ref, defineProps, defineEmits } from "vue";
-const open = ref(true);
+import { ref } from "vue";
+
+import { defineEmits } from "vue";
+
+const open = ref(false);
+const firsht = ref("");
+const fourth = ref("");
+const third = ref("");
+const last = ref("");
 const change = ref("");
-const emit = defineEmits(["editData", "change"]);
-const props = defineProps({
-  updateData: Array,
-});
+const emit = defineEmits(["inputData", "change"]);
+
+function send() {
+  emit("inputData", {
+    name: firsht,
+    subject: fourth,
+    body: third,
+    category: last,
+  });
+}
 function openModal() {
   open.value = !open.value;
 }
-
-const editData = () => {
-  emit("editData", updateData);
-  open.value = false;
-};
 </script>
-
